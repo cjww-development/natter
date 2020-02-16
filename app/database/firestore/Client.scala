c6d84c078f197fb01a58726c87c2e793411e0f29
Chris J W Walker <8091191+chrisjwwalker@users.noreply.github.com>
2020-02-16 10:01:47 +0000

    Basic document get

---
+++ app/database/firestore/Client.scala
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2020 CJWW Development
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package database.firestore
+
+import java.io.InputStream
+
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.cloud.firestore.Firestore
+import com.google.firebase.{FirebaseApp, FirebaseOptions}
+import com.google.firebase.cloud.FirestoreClient
+import javax.inject.{Inject, Named}
+
+class Client @Inject()(@Named("firestoreCreds") serviceAccount: InputStream) {
+
+  private val credentials: GoogleCredentials = GoogleCredentials.fromStream(serviceAccount)
+  private val options: FirebaseOptions = new FirebaseOptions.Builder()
+    .setCredentials(credentials)
+    .build()
+
+  FirebaseApp.initializeApp(options)
+
+  def getDb: Firestore = {
+    FirestoreClient.getFirestore()
+  }
+
+  def getDocument(id: String): Unit = {
+    val docRef = getDb.collection("rooms").document(id)
+    val future = docRef.get()
+    val doc = future.get()
+    if (doc.exists) println("Document data: " + doc.getData)
+    else println("No such document!")
+  }
+
+  getDocument("q2efbUk6YTaI6NipM0hj")
+}
---
+++ app/global/ServiceBindings.scala
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2020 CJWW Development
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package global
+
+import java.io.{FileInputStream, InputStream}
+
+import database.firestore.Client
+import play.api.{Configuration, Environment}
+import play.api.inject.{Binding, Module}
+
+class ServiceBindings extends Module {
+
+  override def bindings(environment: Environment, configuration: Configuration): collection.Seq[Binding[_]] = Seq(
+    bind[InputStream].qualifiedWith("firestoreCreds").to(new FileInputStream(configuration.get[String]("database.firestore.credPath"))).eagerly(),
+    bind[Client].toSelf.eagerly()
+  )
+}
